6aa0a2fa14c32dab274e95789d73c4216bdf72a2
Ely Alamillo <ely0013@gmail.com>
2018-04-16 10:11:34 -0500

    implemented services on component

--- services-assignment-start/src/app/inactive-users/inactive-users.component.ts
+++ services-assignment-start/src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { UserService } from "../services";
 
 @Component({
-  selector: 'app-inactive-users',
-  templateUrl: './inactive-users.component.html',
-  styleUrls: ['./inactive-users.component.css']
+  selector: "app-inactive-users",
+  templateUrl: "./inactive-users.component.html",
+  styleUrls: ["./inactive-users.component.css"]
 })
 export class InactiveUsersComponent {
   @Input() users: string[];
   @Output() userSetToActive = new EventEmitter<number>();
 
+  constructor(private userService: UserService) {}
+  // onSetToActive(id: number) {
+  //   this.userSetToActive.emit(id);
+  // }
+
   onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+    this.userService.userSetToActive(id);
+    this.userService.accountStatusChangedEvent.emit("inactive");
   }
 }
