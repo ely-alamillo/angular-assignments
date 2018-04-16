d6c362743c409b6d2d02dd8ed54124c1b969c971
Ely Alamillo <ely0013@gmail.com>
2018-04-16 10:09:36 -0500

    incorporated userService

--- services-assignment-start/src/app/active-users/active-users.component.ts
+++ services-assignment-start/src/app/active-users/active-users.component.ts
@@ -1,15 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { UserService } from "../services";
 
 @Component({
-  selector: 'app-active-users',
-  templateUrl: './active-users.component.html',
-  styleUrls: ['./active-users.component.css']
+  selector: "app-active-users",
+  templateUrl: "./active-users.component.html",
+  styleUrls: ["./active-users.component.css"]
 })
 export class ActiveUsersComponent {
   @Input() users: string[];
   @Output() userSetToInactive = new EventEmitter<number>();
 
+  constructor(private userService: UserService) {}
+
+  // onSetToInactive(id: number) {
+  //   this.userSetToInactive.emit(id);
+  // }
   onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+    this.userService.userSetToInactive(id);
+    this.userService.accountStatusChangedEvent.emit("active");
   }
 }
