e97b0903504d89711487c2905cfd939b85a420d5
Ely Alamillo <ely0013@gmail.com>
2018-04-16 10:11:00 -0500

    completed user-service

--- services-assignment-start/src/app/services/user-service/user.service.ts
+++ services-assignment-start/src/app/services/user-service/user.service.ts
@@ -1,7 +1,21 @@
-import { Injectable } from "@angular/core";
+import { Injectable, EventEmitter } from "@angular/core";
 
 @Injectable()
 export class UserService {
   activeUsers = ["Max", "Anna"];
   inactiveUsers = ["Chris", "Manu"];
+
+  accountStatusChangedEvent = new EventEmitter<string>();
+
+  userSetToInactive(id: number) {
+    console.log("setting to inactive: ", id);
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+  }
+
+  userSetToActive(id: number) {
+    console.log("setting to active: ", id);
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+  }
 }
